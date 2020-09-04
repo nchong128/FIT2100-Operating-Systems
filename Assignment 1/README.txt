61c083f8e8ac37a91cdeacc2819b892742439a49
Nicholas Lin Ren Chong <ncho0010@student.monash.edu>
2020-09-05 03:19:44 +1200

    Backing up week 5

---
+++ Assignment 1/README.txt
@@ -0,0 +1,16 @@
+FILEUTIL UTILITY C PROGRAM
+AS PART OF: FIT2100 ASSIGNMENT 1 
+WRITTEN BY: NICHOLAS CHONG
+
+COMPILING
+----------------------------------
+In the current directory, enter...
+$ make
+
+This will generate a fileutil file.
+
+USAGE
+----------------------------------
+To use this file...
+
+TBC
--- Assignment 1/fileutil-29808146.c
+++ Assignment 1/fileutil-29808146.c
@@ -5,89 +5,148 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <libgen.h>
 
 int search(char *key, char *arr[], int arrLen);
 char* findFileName(char *dir);
+char* findFileDir(char *filepath) ;
+int isPath(char *path);
+void writeToFile(int infile, int outfile);
 
 int main(int argc, char *argv[]) {
-    int infile, dupfile;
-    ssize_t inbytes, outbytes;
-    size_t nbytes = 1024;
-    char buffer[nbytes], *dir = "logfile.txt", *msg;
-    char *filename;
+    int infile, outfile;
+    char *dir = "./logfile.txt";
+    char *msg;
+    char *filename = "logfile.txt";
+    char *targetdir;
 
+    // PREPROCESSING: CHECK FOR F, d and M FLAGS
+    int fIndex = search("-F", argv, argc);
+    int dIndex = search("-d", argv, argc);
+    int mIndex = search("-M", argv, argc);
+
+    printf("argc:%d\n", argc);
+    printf("d:%d ,M:%d , F:%d\n", dIndex, mIndex, fIndex);
+
+    // PREPROCESSING: Check if directory follows -d flag
+    if (dIndex != -1 && (dIndex + 2 <= argc) && isPath(argv[dIndex+1]) == 0) {
+        targetdir = argv[dIndex+1];
+    }
+    
+    // PREPROCESSING: DECIDE INFILE (EITHER argv[1] OR ./logfile.txt)
     // If file path specified, use argv[1]
     // If no file path specified, uses logfile.txt
-    if (argc > 1) {
-        char firstChar = argv[1][0];
-        if (strcmp(&firstChar, "/" ) == 0) {
-            dir = argv[1];
-        }
-        filename = findFileName(dir);
-        printf("filename: %s\n", filename);
+    if (argc > 1 && isPath(argv[1]) == 0) {
+        dir = argv[1];
     }
 
-    // Opens file
+    filename = findFileName(dir);
+    printf("File name: %s\n", filename);
+
+    // Opens file unless error occurs
     if ((infile = open(dir, O_RDONLY)) < 0) {
-        // close files
-        perror("logfile.txt could not be opened\n");
+        // close file
+        perror("File could not be opened");
         exit(1);
     }
 
+
+    // MAIN ACTIONS 
+    if (dIndex != -1) {
+        // -d present
+        char* outpath = strcat(strcat(targetdir, "/"), filename);
+        printf("%s", outpath);
+
+        // make output file
+        if ((outfile = open(outpath, O_WRONLY | O_CREAT | O_EXCL, 0664)) < 0) {
+            // close files
+            perror("Destination directory already has a file with the same name as the source file OR directory does not exist");
+            exit(2);
+        }
+
+        writeToFile(infile, outfile);
+
+        msg = "Copy successful\n";
+        write(1, msg, strlen(msg));
+
+        
+        // if (mIndex != -1) {
+
+        // }
+
+
+    } else if (mIndex == -1 && fIndex == -1) {
+        // No arguments present 
+        // PRINT FILE CONTENTS TO STDOUT
+        writeToFile(infile, 1);        
+    } else if (fIndex != -1) {
+        // -F present only
+        perror("Invalid argument, -F is redundant as nothing to force here!");
+        exit(1);
+    } else {
+        // -F OR -M present only
+        perror("Invalid argument, no destination to move the file!");
+        exit(1);
+    }
+
+
     // TASK 2
     // Loop over file and print outputs
-    while ((inbytes = read(infile, buffer, nbytes)) != 0) {
-        outbytes = write(1, buffer, inbytes);
-
-        if (outbytes == 0) {
-            break;
-        }
-    }
-    write(1, "\n", 2);
 
 
+    /*
     // TASK 3
     int dPos = search("-d", argv, argc);
-    printf("%d\n", dPos);
     if (argc > 2) {
         char firstChar = argv[dPos+1][0];
         if (strcmp(&firstChar, "/" ) == 0) {
             // append file name to end
             // char targetDir = strcat(argv[dPos+1], );
 
-            if ((dupfile = open("/mnt/c/Users/nchon/Documents/FIT2100/Assignment\\ 1/empty.txt", O_WRONLY | O_CREAT | O_EXCL)) < 0) {
+
+            if ((outFile = open("/mnt/c/Users/nchon/Documents/FIT2100/Assignment\\ 1/empty.txt", O_WRONLY | O_CREAT | O_EXCL)) < 0) {
                 // close files
                 perror("Destination directory already has a file with the same name as the source file OR directory does not exist");
                 exit(2);
             }
 
-            // Copy infile to dupfile
+            // Copy infile to outFile
             lseek(infile, 0, SEEK_SET);
             while ((inbytes = read(infile, buffer, nbytes)) != 0) {
-                outbytes = write(dupfile, buffer, inbytes);
+                outbytes = write(outFile, buffer, inbytes);
                 if (outbytes == 0) break;
             }
             
         }
+        msg = "Copy successful\n";
+        write(1, msg, strlen(msg));
     }
 
-
-
-    msg = "Copy successful\n";
-    write(1, msg, strlen(msg));
+    */
 
     close(infile);
-    close(dupfile);
+    close(outfile);
     exit(0);
 
+}
+
+void writeToFile(int infile, int outfile) {
+    ssize_t inbytes, outbytes;
+    size_t nbytes = 1024;
+    char buffer[nbytes];
+
+    lseek(infile, 0, SEEK_SET);
+    while ((inbytes = read(infile, buffer, nbytes)) != 0) {
+        outbytes = write(outfile, buffer, inbytes);
+
+        if (outbytes == 0) break;
+    }
 }
 
 int search(char *key, char *arr[], int arrLen) {
     // Returns first located index of key
     // If not in arr, returns -1
-    int i;
-
-    for (i = 0; i < arrLen; i++) {
+    for (int i = 0; i < arrLen; i++) {
         if (strcmp(key, arr[i]) == 0 ) {
             return i;
         }
@@ -95,17 +154,15 @@ int search(char *key, char *arr[], int arrLen) {
     return -1;
 }
 
-char* findFileName(char *dir) {
-    // finds the last backslash in the dir string
-    // returns a copy of the string onwards
-    dir = "/mnt/c/Users/nchon/Documents/FIT2100/Assignment\\ 1/empty.txt";
-    char *filename = "";
+char* findFileName(char *filepath) {
+    return basename(strdup(filepath));
+}
 
-    for (int i = strlen(dir)-1; i >= 0; i-- ) {
-        if (strcmp(&dir[i],"/") == 0) {
-            return strncpy(filename, dir+i+1, strlen(dir)-i-1);
-        }
-    }
-    
-    return filename;
+char* findFileDir(char *filepath) {
+    return dirname(strdup(filepath));
+}
+
+int isPath(char *path) {
+    char firstChar = path[0];
+    return strcmp(&firstChar, "/");
 }
