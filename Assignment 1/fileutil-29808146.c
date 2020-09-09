42871638858cc6ede25e93f42b0f0e6a07653400
Nicholas Lin Ren Chong <ncho0010@student.monash.edu>
2020-09-10 01:03:24 +1200

    Backing up week 6

--- Assignment 1/fileutil-29808146.c
+++ Assignment 1/fileutil-29808146.c
@@ -9,13 +9,21 @@
 int search(char *key, char *arr[], int arrLen);
 int isPath(char *path);
 void writeToFile(int infile, int outfile);
+int invalidFlagsExist(int argc, char *argv[]);
 
 int main(int argc, char *argv[]) {
     int infile, outfile;
     char *dir = "./logfile.txt";
-    char *msg;
     char *filename = "logfile.txt";
+    char *msg;
     char *targetdir;
+    
+    // PREPROCESSING: Check for invalid flags
+    if (invalidFlagsExist(argc, argv) == 1) {
+        msg = "fileutil: invalid option\n";
+        write(2, msg, strlen(msg));
+        exit(1);
+    }
 
     // PREPROCESSING: CHECK FOR F, d and M FLAGS
     int fIndex = search("-F", argv, argc);
@@ -23,9 +31,15 @@ int main(int argc, char *argv[]) {
     int mIndex = search("-M", argv, argc);
 
     // PREPROCESSING: Check if directory follows -d flag
-    if (dIndex != -1 && (dIndex + 2 <= argc) && isPath(argv[dIndex+1]) == 0) {
-        targetdir = argv[dIndex+1];
-    }
+    if (dIndex != -1) {
+        if ((dIndex + 2 <= argc) && isPath(argv[dIndex+1]) == 0) {
+            targetdir = argv[dIndex+1];
+        } else {
+            msg  = "fileutil: immediately after -d, a directory path was expected\n";
+            write(2, msg, strlen(msg));
+            exit(1);
+        }
+    } 
     
     // PREPROCESSING: DECIDE INFILE (EITHER argv[1] OR ./logfile.txt)
     // If file path specified, use argv[1]
@@ -39,7 +53,7 @@ int main(int argc, char *argv[]) {
     // Opens file unless error occurs
     if ((infile = open(dir, O_RDONLY)) < 0) {
         // close file
-        msg = "File could not be opened";
+        msg = "fileutil: file could not be opened\n";
         write(2, msg, strlen(msg));
         exit(1);
     }
@@ -49,15 +63,17 @@ int main(int argc, char *argv[]) {
         // -d present
         char* outpath = strcat(strcat(targetdir, "/"), filename);
 
-        // make output file
-        while ((outfile = open(outpath, O_WRONLY | O_CREAT | O_EXCL, 0664)) < 0) {
+        // open output file
+        while ((outfile = open(outpath, O_WRONLY | O_CREAT | O_EXCL, 00700)) < 0) {
             if (fIndex != -1) {
                 // if in force mode, unlink file
+                msg = "fileutil: FORCE MODE ACTIVE\n";
+                write(1, msg, strlen(msg));
                 close(outfile);
                 unlink(outpath);
             } else {
                 // close files
-                msg = "Destination directory already has a file with the same name as the source file OR directory does not exist";
+                msg = "fileutil: destination directory already has a file with the same name as the source file OR directory does not exist\n";
                 write(2, msg, strlen(msg));
                 exit(2);
             }
@@ -67,25 +83,24 @@ int main(int argc, char *argv[]) {
 
         if (mIndex != -1) {
             unlink(dir);
-            msg = "Move successful\n";
+            msg = "fileutil: move successful\n";
             write(1, msg, strlen(msg));
         } else {
-            msg = "Copy successful\n";
+            msg = "fileutil: copy successful\n";
             write(1, msg, strlen(msg));
         }
-
     } else if (mIndex == -1 && fIndex == -1) {
         // No arguments present 
         // PRINT FILE CONTENTS TO STDOUT
         writeToFile(infile, 1);        
     } else if (fIndex != -1) {
         // -F present only
-        msg = "Invalid argument, -F is redundant as nothing to force here!";
+        msg = "fileutil: invalid argument, -F is redundant as nothing to force here! \n";
         write(2, msg, strlen(msg));
         exit(1);
     } else {
         // -F OR -M present only
-        msg = "Invalid argument, no destination to move the file!";
+        msg = "fileutil: invalid argument, no destination to move the file! \n";
         write(2, msg, strlen(msg));
         exit(1);
     }
@@ -122,4 +137,29 @@ int search(char *key, char *arr[], int arrLen) {
 int isPath(char *path) {
     char firstChar = path[0];
     return strcmp(&firstChar, "/");
+}
+
+int invalidFlagsExist(int argc, char *argv[]) {
+    int fCount, dCount, mCount;
+
+    for (int i = 1; i < argc; i++) {
+        if ((((isPath(argv[i]) == 0) && i != 1) && (strcmp("-d", argv[i-1]) != 0)) 
+            && (strcmp("-d", argv[i]) != 0) 
+            && (strcmp("-M", argv[i]) != 0) 
+            && (strcmp("-F", argv[i]) != 0)) {
+                return 1;
+        }
+
+        if (strcmp("-F", argv[i]) == 0) {
+            fCount += 1;
+        } else if (strcmp("-M", argv[i]) == 0) {
+            mCount += 1;
+        } else if (strcmp("-d", argv[i]) == 0) {
+            dCount += 1;
+        }
+
+        if (fCount > 1 || dCount > 1 || mCount > 1) return 1;
+    }
+
+    return 0;
 }
